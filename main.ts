e0ffb9012ad236ca308e354dc2b2841e76d68c6d
410775027 <71691336+410775027@users.noreply.github.com>
2020-10-06 15:48:19 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+for (let a = 0; a <= 4; a++) {
+    led.plot(0, a)
+    led.plot(4, a)
+    led.plot(a, 0)
+    led.plot(a, 4)
+    basic.pause(500)
+}
+basic.forever(function () {
+    for (let index = 0; index <= 4; index++) {
+        led.plotBarGraph(
+        index,
+        index
+        )
+    }
+})
